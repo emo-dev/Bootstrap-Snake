a6c7511c208185a38ecd488376802cce86b9caf2
Taylor Perkinse <perkinstaylor.luthier@gmail.com>
2017-03-15 09:37:31 -0500

    added closing tag.. Thanks adam

--- BootstrapSnake/app/factories/GameBoardFactory.js
+++ BootstrapSnake/app/factories/GameBoardFactory.js
@@ -5,7 +5,7 @@ app.factory("GameboardFactory", function() {
 	let setupGameboard = (rowCount) => {
 		return new Promise((resolve, reject) => {
 
-			$( "<div class='snakeContainer col-xs-8 col-xs-offset-2 text-center'></div" ).appendTo( ".well" );
+			$( "<div class='snakeContainer col-xs-8 col-xs-offset-2 text-center'></div>" ).appendTo( ".well" );
 			for (var row = 0; row < rowCount; row++) {
 				$("<div class='row snakerow'></div>").appendTo( ".snakeContainer" );
 				for (var checkbox = 0; checkbox < rowCount; checkbox++) {
